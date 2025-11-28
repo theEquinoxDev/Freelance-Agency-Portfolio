0d259eb6aa0408ef3b3de7cd9d57685268aaa598
Aditya <adityasuri11ss@gmail.com>
2025-11-28 15:30:17 +0530

    revisions by client

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import Hero from "./sections/Hero";
 import Services from "./sections/Services";
 import Portfolio from "./sections/Portfolio";
 import Testimonials from "./sections/Testimonials";
 import CTA from "./sections/CTA";
 import Footer from "./sections/Footer";
+import CursorFollower from "./components/CursorFollower";
+import ScrollProgress from "./components/ScrollProgress";
+import Marquee from "./components/Marquee";
 
 export default function App() {
   return (
     <>
-      <Navbar />
+      <ScrollProgress />
+      <CursorFollower />
+      <Sidebar />
       <Hero />
+      <Marquee />
       <Services />
       <Portfolio />
       <Testimonials />
